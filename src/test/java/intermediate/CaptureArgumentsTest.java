88174a3e724e7318673e1af49b29548dfcfe032d
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-04-13 21:10:23 +0200

    Added EasyMock example for ArgumentCapture

--- src/test/java/intermediate/CaptureArgumentsTest.java
+++ src/test/java/intermediate/CaptureArgumentsTest.java
@@ -1,5 +1,9 @@
 package intermediate;
 
+import static org.easymock.EasyMock.capture;
+import static org.easymock.EasyMock.createNiceMock;
+import static org.easymock.EasyMock.newCapture;
+import static org.easymock.EasyMock.replay;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
@@ -7,6 +11,7 @@ import static org.mockito.Mockito.verify;
 
 import java.util.function.Function;
 
+import org.easymock.Capture;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
@@ -17,6 +22,29 @@ import business.Result;
 
 @RunWith(Enclosed.class)
 public class CaptureArgumentsTest {
+
+
+    public static class EasyMock {
+
+        @SuppressWarnings("unchecked")
+        private final Function<Result, Void> callback = createNiceMock(Function.class);
+        private final BusinessLogic logic = new BusinessLogic();
+
+        @Test
+        public void captureArgumentPassedToAMock() throws Exception {
+            Capture<Result> resultCapture = newCapture();
+            callback.apply(capture(resultCapture));
+            org.easymock.EasyMock.expectLastCall().andStubReturn(null);
+            replay(callback);
+
+            logic.onSuccess(callback);
+            logic.runLogic();
+
+            org.easymock.EasyMock.verify(callback);
+
+            assertThat(resultCapture.getValue().message, is("Everything was fine"));
+        }
+    }
 
     public static class Mockito {
 
