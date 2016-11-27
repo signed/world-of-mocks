05107e033e0aef50e845d7c9eba463f385b53d18
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-11-27 22:34:29 +0100

    improve exampleπ

--- mockito/src/test/java/sample/TwoDotXTest.java
+++ mockito/src/test/java/sample/TwoDotXTest.java
@@ -11,6 +11,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockito.junit.VerificationCollector;
+import org.mockito.stubbing.Answer1;
 import org.mockito.stubbing.Answer2;
 
 import java.math.BigDecimal;
@@ -135,7 +136,7 @@ public class TwoDotXTest {
 
         void two(String one, int two);
 
-        Long oneReturingLong(int one);
+        Long oneReturningLong(int one);
 
         BigDecimal twoReturningBigDecimal(int one, StringBuilder builder);
     }
@@ -145,13 +146,13 @@ public class TwoDotXTest {
 
     @Test
     public void new_strongly_typed_answers() throws Exception {
-        when(answers.oneReturingLong(anyInt())).thenAnswer(AdditionalAnswers.answer(arg -> 42L));
-        assertThat(answers.oneReturingLong(-7), equalTo(42L));
+        when(answers.oneReturningLong(anyInt())).thenAnswer(AdditionalAnswers.answer((Answer1<Long, Integer>) argument0 -> 42L + argument0));
+        assertThat(answers.oneReturningLong(5), equalTo(47L));
 
         when(answers.twoReturningBigDecimal(anyInt(), any())).thenAnswer(AdditionalAnswers.answer((Answer2<BigDecimal, Integer, StringBuilder>) (argument0, argument1) -> new BigDecimal(argument0)));
         assertThat(answers.twoReturningBigDecimal(7, null), equalTo(BigDecimal.valueOf(7L)));
 
-        doAnswer(AdditionalAnswers.answer((String arg) -> {throw new RuntimeException(arg);})).when(answers).one("message");
+        doAnswer(AdditionalAnswers.answerVoid((String arg) -> {throw new RuntimeException(arg);})).when(answers).one("message");
         try{
             answers.one("message");
             Assert.fail("expected exceptions");
