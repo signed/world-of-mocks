758c3221b216714abfc3a138bfde4480e2e85843
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-11-28 23:23:48 +0100

    add some samples for any matcher

---
+++ mockito/src/test/java/sample/_1/MatchersTest.java
@@ -0,0 +1,46 @@
+package sample._1;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MatchersTest {
+
+    @SuppressWarnings("unchecked")
+    private final List<String> listOfString = mock(List.class);
+
+    @SuppressWarnings("unchecked")
+    private final List<Object> listOfObject = mock(List.class);
+
+    @Test
+    public void hamcrest_matcher_for_arguments() throws Exception {
+        when(listOfString.add(Mockito.argThat(equalTo("example")))).thenReturn(true);
+
+        assertThat("Should not match", !listOfString.add("unknown"));
+        assertThat("Should match", listOfString.add("example"));
+    }
+
+    @Test
+    public void any_matcher() throws Exception {
+        when(listOfObject.add(Mockito.any())).thenReturn(true);
+
+        assertThat("should match", listOfObject.add(null));
+        assertThat("should match", listOfObject.add("a string"));
+        assertThat("should match", listOfObject.add(45));
+    }
+
+    @Test
+    public void any_type_matcher() throws Exception {
+        when(listOfObject.add(Mockito.anyInt())).thenReturn(true);
+
+        assertThat("should match", listOfObject.add(null));
+        assertThat("should match", listOfObject.add(42));
+        assertThat("should not match", listOfObject.add(42L));
+    }
+}
