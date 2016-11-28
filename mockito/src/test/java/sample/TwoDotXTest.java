fe1878b1c6947165e04183ec19cf2ed940784360
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-11-28 22:57:01 +0100

    move unused stubbing example into own class

--- mockito/src/test/java/sample/TwoDotXTest.java
+++ mockito/src/test/java/sample/TwoDotXTest.java
@@ -1,35 +1,14 @@
 package sample;
 
 import org.junit.Ignore;
-import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
-
-import java.util.List;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.when;
 
 @Ignore
 public class TwoDotXTest {
-
-    @Rule
-    public MockitoRule rule = MockitoJUnit.rule();
-
-    @Mock
-    private List<String> list;
-
-    @Test
-    public void hint_on_unused_stubbing() throws Exception {
-        when(list.get(anyInt())).thenReturn("something");
-        when(list.add(anyString())).thenReturn(true);
-    }
 
     private static abstract class Abstract {
 
---
+++ mockito/src/test/java/sample/_2/UnusedStubbingTest.java
@@ -0,0 +1,28 @@
+package sample._2;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.List;
+
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+public class UnusedStubbingTest {
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @Mock
+    private List<String> list;
+
+    @Test
+    public void hint_on_unused_stubbing() throws Exception {
+        when(list.get(anyInt())).thenReturn("something");
+        when(list.add(anyString())).thenReturn(true);
+    }
+}
