aa8335df48b38b97b19a75282c537f8e6f98ef07
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-11-30 23:53:50 +0100

    add exception chaining example

---
+++ mockito/src/test/java/sample/_2/ApiImprovementsTest.java
@@ -0,0 +1,27 @@
+package sample._2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ApiImprovementsTest {
+
+    @Test
+    public void simpler_setup_to_throw_exceptions() throws Exception {
+        Runnable runnable = Mockito.mock(Runnable.class);
+        Mockito.doThrow(UnsupportedOperationException.class).doThrow(IllegalStateException.class).when(runnable).run();
+
+        try {
+            runnable.run();
+            Assert.fail();
+        } catch (UnsupportedOperationException e) {
+
+        }
+
+        try {
+            runnable.run();
+            Assert.fail();
+        } catch (IllegalStateException e) {
+        }
+    }
+}
