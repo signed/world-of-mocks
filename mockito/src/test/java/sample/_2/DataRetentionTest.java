f993651db22152beca57282ef6cb4f67a5856d55
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-12-05 23:50:16 +0100

    extend example

--- mockito/src/test/java/sample/_2/DataRetentionTest.java
+++ mockito/src/test/java/sample/_2/DataRetentionTest.java
@@ -4,6 +4,7 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
@@ -24,8 +25,14 @@ public class DataRetentionTest {
     private final Class<?> type = mock(Sample.class).getClass();
 
     @Test
-    public void retainGenericTypeInformation() throws Exception {
-        assertThat("should not be present", type.isAnnotationPresent(Ignore.class));
+    public void retainTypeAnnotations() throws Exception {
+        assertThat("should be present on mocked class", type.isAnnotationPresent(Ignore.class));
+    }
+
+    @Test
+    public void retainMethodAnnotations() throws Exception {
+        Method sample = type.getDeclaredMethod("sample");
+        assertThat("should be present on mocked method", sample.isAnnotationPresent(Ignore.class));
     }
 
     @Test
